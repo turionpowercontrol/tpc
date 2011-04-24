765509fa3e04f0baa3037a50f0a73b9d1380e11c
kszysiu <kszysiu@bee53e60-5692-9632-f597-c23173791608>
2011-04-24 21:08:57 +0000

    Remove legacy build scripts.

--- compile_i386.sh
+++
@@ -1,1 +0,0 @@
-c++ -m32 -D_FILE_OFFSET_BITS=64 TurionPowerControl.cpp cpuPrimitives.cpp Processor.cpp config.cpp scaler.cpp Griffin.cpp K10Processor.cpp MSVC_Round.cpp -o ../bin/TurionPowerControl
--- compile_x86-64.sh
+++
@@ -1,1 +0,0 @@
-c++ -m64 TurionPowerControl.cpp cpuPrimitives.cpp Processor.cpp config.cpp scaler.cpp Griffin.cpp K10Processor.cpp MSRObject.cpp PCIRegObject.cpp MSVC_Round.cpp -o ../bin/TurionPowerControl
