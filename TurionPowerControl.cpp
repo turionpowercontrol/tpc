376373cdd68bc86d49636d250f81c933dc6ad241
paolo.sabatino <paolo.sabatino@gmail.com>
2012-04-17 15:43:18 +0000

    Solved confict for version naming

--- TurionPowerControl.cpp
+++ TurionPowerControl.cpp
@@ -773,7 +773,7 @@ int main (int argc,const char **argv) {
 	
 	Scaler *scaler;
 	
-	printf ("Turion Power States Optimization and Control - by blackshard - v0.425\n\n");
+	printf ("Turion Power States Optimization and Control - by blackshard - v0.43\n\n");
 
 	if (argc<2) {
 		printUsage(argv[0]);
