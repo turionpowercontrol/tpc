a9f73b757e37895deb708249f65811c1fb1ee725
kszysiu <kszysiu@bee53e60-5692-9632-f597-c23173791608>
2014-11-21 18:19:29 +0000

    Support PCI ECS access on Windows XP/2003 [1/4]
    Implement PCI Extended Configuration Space (ECS) access using AMD's
    IOCF8/IOCFC method (enabled by NB_CFG/EnableCf8ExtCfg).
    
    Use custom implementation for ECS accesses on XP/2003. In all other
    cases use original WinRing0 implementation.
    
    New, OS-agnostic PCI register access functions are now:
    SysReadPciConfigDwordEx and SysWritePciConfigDwordEx.
    
    Other PCI register access functions have not been ported due to lack
    of users.

--- sysdep-win32.cpp
+++ sysdep-win32.cpp
@@ -90,3 +90,129 @@ void ClearScreen(unsigned int flags)
 	}
 	SetConsoleCursorPosition(h, corner);
 }
+
+//
+// Tests if special PCI Extended Configuration Space access is required.
+//
+// This is true on Windows XP/2003 and configuration space offsets >= 0x100
+// as these operating systems do not provide appropriate API.
+// 
+// More info:
+//   http://www.osronline.com/showThread.cfm?link=87866
+//   https://code.google.com/p/turionpowercontrol/issues/detail?id=28
+//
+bool SpecialEcsAccessRequired(DWORD reg)
+{
+	OSVERSIONINFO versioninfo;
+	if (reg < 0x100)
+		return false;
+	versioninfo.dwOSVersionInfoSize = sizeof(versioninfo);
+	if (!GetVersionEx(&versioninfo)) {
+		return false;
+	}
+	if (versioninfo.dwMajorVersion >= 6) {
+		return false;
+	}
+	return true;
+}
+
+//
+// Performs special Extended Configuration Space access.
+//
+// It takes advantage of AMD-specific IO CF8h/CFCh extension that enables
+// accesses to PCI Extended Configuration Space. Access is realized by
+// enabling ECS access via IOCF8/IOCFC using EnableCf8ExtCfg from NB_CFG
+// MSR (MSRC001_001F) and performing CF8h/CFCh access per specifciation.
+// State of NB_CFG and IOCF8 is saved before the operation and gets
+// restored afterwards. Calling process is also bound to first CPU
+// in the system for the duration of the operation to accommodate
+// environments with multiple northbridges.
+// 
+// EnableCf8ExtCfg is supported by all currently supported CPU families,
+// that is, 10h, 11h, 12h, 14h, 15h and 16h.
+//
+// IO CF8h/CFCh method, while racy, is the only feasible method to access
+// PCI ECS on Windows XP/2003 (accessing memory mapped configuration space
+// is off the table due to lack of physical memory access support
+// in WinRing0).
+//
+// Best effort is put to detect simultaneous users of CF8h/CFCh.
+//
+bool SpecialEcsAccess(bool write, DWORD devfunc, DWORD reg, DWORD *res)
+{
+	DWORD_PTR mask_save;
+	DWORD_PTR dummy;
+	DWORD eax_save;
+	DWORD edx_save;
+	DWORD edx;
+	DWORD addr;
+	DWORD addr_save;
+	DWORD addr_check;
+	DWORD data;
+	bool result = false;
+
+	if (!GetProcessAffinityMask((HANDLE)-1, &mask_save, &dummy)) {
+		fprintf(stderr, "ERROR getting affinity mask\n");
+		goto out;
+	}
+	if (!SetProcessAffinityMask((HANDLE)-1, 1)) {
+		fprintf(stderr, "ERROR setting affinity mask\n");
+		goto out;
+	}
+	if (!RdmsrPx(0xC001001F, &eax_save, &edx_save, 1)) {
+		fprintf(stderr, "ERROR reading NB_CFG\n");
+		goto out_affinity;
+	}
+	edx = edx_save;
+	edx |= 0x4000;
+	if (!WrmsrPx(0xC001001F, eax_save, edx, (DWORD_PTR)1)) {
+		fprintf(stderr, "ERROR writing NB_CFG\n");
+		goto out_affinity;
+	}
+	addr_save = ReadIoPortDword(0xcf8);
+	addr = 1;
+	addr <<= 7;
+	addr |= (reg >> 8) & 0x0F;
+	addr <<= 16;
+	addr |= devfunc & 0xFFFF;
+	addr <<= 8;
+	addr |= reg & 0xFF;
+	WriteIoPortDword(0xcf8, addr);
+	if (write == false) {
+		data = ReadIoPortDword(0xcfc);
+	} else {
+		WriteIoPortDword(0xcfc, *res);
+	}
+	addr_check = ReadIoPortDword(0xcf8);
+	if (addr_check != addr) {
+		fprintf(stderr, "ERROR: IO CF8h hijacked!\n");
+		goto out_nbcfg;
+	}
+	WriteIoPortDword(0xcf8, addr_save);
+	if (write == false) {
+		*res = data;
+	}
+	result = true;
+out_nbcfg:
+	WrmsrPx(0xC001001F, eax_save, edx_save, (DWORD_PTR)1);
+out_affinity:
+	SetProcessAffinityMask((HANDLE)-1, mask_save);
+out:
+	return result;
+}
+
+BOOL SysReadPciConfigDwordEx(DWORD pciAddress, DWORD regAddress, PDWORD value)
+{
+	if (SpecialEcsAccessRequired(regAddress)) {
+	        return SpecialEcsAccess(false, pciAddress, regAddress, value);
+	}
+	return ReadPciConfigDwordEx(pciAddress, regAddress, value);
+}
+
+BOOL SysWritePciConfigDwordEx(DWORD pciAddress, DWORD regAddress, DWORD value)
+{
+	if (SpecialEcsAccessRequired(regAddress)) {
+	        return SpecialEcsAccess(true, pciAddress, regAddress, &value);
+	}
+	return WritePciConfigDwordEx(pciAddress, regAddress, value);
+}
